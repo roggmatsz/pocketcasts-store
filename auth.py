cf3ebab6404783c520b42fd0eaa1cdd4af9393fd
Rogger Matamoros <rogger@roggmatz.com>
2024-12-20 18:18:43 -0500

    Initial commit. Moved auth-related code to separate file. Tidies up main.

---
+++ auth.py
@@ -0,0 +1,26 @@
+import json
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def do_login(http, user, pw):
+    if not user or not pw:
+        return None
+
+    data = {"email": f"{user}", "password": f"{pw}", "scope": "webplayer"}
+    encoded_data = json.dumps(data).encode("utf-8")
+    print(encoded_data)
+    http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
+    response = http.request(
+        "POST",
+        "https://api.pocketcasts.com/user/login",
+        headers={"Content-Type": "application/json"},
+        body=encoded_data,
+    )
+    token = json.loads(response.data)["token"]
+    print(token)
+    return token
+
+
+def create_auth_headers(token):
+    return {"Authorization": f"Bearer {token}"}
