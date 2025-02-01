1b0b3503cc68039b7c76011a204c4847450a2075
Rogger Matamoros <rogger@roggmatz.com>
2025-02-01 14:14:17 -0500

    First working test that confirms the right number of columns were created. Needs more refinement.

--- tests/test_sqlite_store.py
+++ tests/test_sqlite_store.py
@@ -2,5 +2,12 @@ import pytest
 from src.sqlite_store import SQLiteStore
 
 def test_sqlite_store():
-    
-    pass
+    store = SQLiteStore(':memory:')
+    cursor = store.db_connection.cursor()
+
+    cursor.execute(f'PRAGMA table_info(Listening_History)')
+    columns = cursor.fetchall()
+
+    # column[1] contains the string names of the columns in the table.
+    column_names = [ column[1] for column in columns ]
+    assert len(column_names) == 12
