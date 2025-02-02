c0eb0d78b011445d77719418bd390bb91b544c6b
Rogger Matamoros <rogger@roggmatz.com>
2025-02-01 20:08:04 -0500

    Created fixture for data.json file. Added working test for save_records.

--- tests/test_sqlite_store.py
+++ tests/test_sqlite_store.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 
 from src.sqlite_store import SQLiteStore
 
@@ -7,6 +8,11 @@ def data_store():
     store = SQLiteStore(':memory:')
     yield store
     store.close()
+
+@pytest.fixture
+def dataset1():
+    with open('tests/data.json') as file:
+        return json.load(file)
     
 
 def test_table_creation(data_store):
@@ -19,6 +25,12 @@ def test_table_creation(data_store):
     column_names = [ column[1] for column in columns ]
     assert len(column_names) == 12
 
-def test_save_records():
-    
-    pass
+def test_save_records(data_store, dataset1):
+    records_saved: int = data_store.save_records(dataset1['episodes'])
+
+    cursor = data_store.db_connection.cursor()
+    cursor.execute('SELECT * FROM Listening_History')
+    records = cursor.fetchall()
+
+    assert records_saved == len(dataset1['episodes'])
+    assert len(records) == len(dataset1['episodes'])
