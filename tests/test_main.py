cf570b4ed9cbc4790eda39b344a31efd29376d35
Rogger Matamoros <rogger@roggmatz.com>
2025-02-01 22:40:37 -0500

    Added working test to confirm new diffing function works as the old one did.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,9 +2,12 @@ from typing import Any
 from src.main import diff_records
 from src.sqlite_store import SQLiteStore
 
-def test_diff_records(data_store: SQLiteStore, dataset2: dict, dataset3: dict):
+def test_diff_records(data_store: SQLiteStore, dataset2: dict, dataset3: dict, dataset4: dict):
     data_store.save_records(dataset2['episodes'])
     existing_records = data_store.get_records()
-
     # value comes from diff of dataset2 and 3 based on working code.
     assert len(diff_records(dataset3['episodes'], existing_records)) == 22
+
+    data_store.save_records(dataset3['episodes'])
+    existing_records = data_store.get_records()
+    assert len(diff_records(dataset4['episodes'], existing_records)) == (56 + 22)
