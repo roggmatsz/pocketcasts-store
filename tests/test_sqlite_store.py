d3ceb45af45b0ccb2ebd48b7020852fa347d7c7d
Rogger Matamoros <rogger@roggmatz.com>
2025-02-01 20:20:27 -0500

    Added working test for get_records.

--- tests/test_sqlite_store.py
+++ tests/test_sqlite_store.py
@@ -34,3 +34,20 @@ def test_save_records(data_store, dataset1):
 
     assert records_saved == len(dataset1['episodes'])
     assert len(records) == len(dataset1['episodes'])
+
+def test_get_records(data_store, dataset1):
+    data_store.save_records(dataset1['episodes'])
+
+    records = data_store.get_records()
+
+    assert len(records) == len(dataset1['episodes'])
+    assert records[0][0] == dataset1['episodes'][-1]['uuid']
+    assert records[0][1] == dataset1['episodes'][-1]['url']
+    assert records[0][2] == dataset1['episodes'][-1]['published']
+    assert records[0][3] == dataset1['episodes'][-1]['duration']
+    assert records[0][4] == dataset1['episodes'][-1]['title']
+    assert records[0][5] == int(dataset1['episodes'][-1]['size'])
+    assert records[0][6] == dataset1['episodes'][-1]['starred']
+    assert records[0][7] == dataset1['episodes'][-1]['podcastUuid']
+    assert records[0][8] == dataset1['episodes'][-1]['podcastTitle']
+    assert records[0][9] == dataset1['episodes'][-1]['author']
