3e9035e8155bb273baa54e233c2ef90a96415d19
Rogger Matamoros <rogger@roggmatz.com>
2025-02-01 20:49:34 -0500

    Updated scope so json files are set for the entire session and the store for the module being tested.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,17 @@ import json
 from src.sqlite_store import SQLiteStore
 
 @pytest.fixture
-def data_store():
+def data_store(scope='module'):
     store = SQLiteStore(':memory:')
     yield store
     store.close()
 
 @pytest.fixture
-def dataset1():
+def dataset1(scope='session'):
     with open('tests/data.json') as file:
         return json.load(file)
     
 @pytest.fixture
-def dataset2():
+def dataset2(scope='session'):
     with open('tests/data1.json') as file:
         return json.load(file)
